1bdbd7a28f544def8b8aa166e5d5e72cdde49ec1
Milan <milan.simo.ilic@gmail.com>
2022-06-05 13:44:12 +0200

    [mozzart-widget]-inicijalne-rute-ilic

--- src/router.js
+++ src/router.js
@@ -2,7 +2,14 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {path: '/fudbal', component: null},
+        {path: '/kosarka', component: null},
+        {path: '/tenis', component: null},
+        {path: '/hokej', component: null},
+        {path: 'rukomet', component: null},
+        {path: '/odbojka', component: null}
+    ]
 })
 
 export default router;
